01618ac4b7f6ee8fcad5c77cd5ba38b5cd5beb54
mars_000 <97606588+shivamyadav2002@users.noreply.github.com>
2022-09-27 02:06:49 +0530

    Update singly_linked_list.cpp

--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -6,12 +6,29 @@ class node{
     public:
     int data;
     node *next;
+    node(int data)
+    {
+        this->data=data;
+        this->next=NULL;
+    }
+    //destructor
+    ~node(){                                //ye destructor nahi samaj aa raha hai??????
+        int value=this->data;
+        //memory free
+        if(this->next!=NULL)
+        {
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"memory is free for node with data:"<<value<<endl;
+    }
 };
 int size_of_list(node * &head)
 {
     int size=0;
-    node *temp=new node;
-    temp=head;
+    //node *temp=new node;             
+    //temp=head;
+    node *temp=head;
     while(temp!=NULL)
     {
         size++;
@@ -21,16 +38,18 @@ int size_of_list(node * &head)
 }
 void insert_at_head(int d,node* &head)
 {
-    node *temp=new node;
-    temp->data=d;
+    //node *temp=new node;                  
+    //temp->data=d;
+    node* temp=new node(d);
     temp->next=head;
     head=temp;
 }
 void insert_at_tail(int d,node* &tail)
 {
-    node* temp=new node;
-    temp->data=d;
-    temp->next=NULL;
+    //node* temp=new node;                  
+    //temp->data=d;
+    //temp->next=NULL;
+    node* temp=new node(d);
     tail->next=temp;
     tail=temp;
 }
@@ -40,10 +59,12 @@ void insertAtPos(int pos,int d,node* &head,node* &tail)//it will work for any ge
         insert_at_head(d,head);
     else{
         
-        node *temp1=new node;
-        node *temp=new node;
-        node *middle=new node;
-        middle->data=d;
+        //node *temp1=new node;             
+        //node *temp=new node;
+        node *temp1,*temp;
+        //node *middle=new node;             
+        //middle->data=d;
+        node *middle=new node(d);
         temp=head;
         for(int i=1;i<=pos-2;++i)
             temp=temp->next;
@@ -56,8 +77,9 @@ void insertAtPos(int pos,int d,node* &head,node* &tail)//it will work for any ge
 }
 void print(node* &head)//traversing singly linked list
 {
-    node *temp=new node;
-    temp=head;
+    //node *temp=new node;                !!!
+    //temp=head;
+    node* temp=head;
     while(temp!=NULL)
     {
         cout<<temp->data<<" ";
@@ -65,13 +87,40 @@ void print(node* &head)//traversing singly linked list
     }
     cout<<endl;
 }
+void deleteAt(int pos,node* &head,node* &tail)
+{
+    //node* temp=new node;            !!!
+    //temp=head;
+    node* temp=head;
+    if(pos==1)//memory free ??
+    {
+        head=head->next;
+        temp->next=NULL;
+        delete temp;
+    }
+    else{
+        for(int i=1;i<pos-1;++i)
+            temp=temp->next;
+        //node* temp1=new node;       !!!
+        //temp1=temp->next;
+        node* temp1=temp->next;
+        //node* temp2=new node;       !!!
+        //temp2=temp1->next;
+        node* temp2=temp1->next; 
+        temp->next=temp2;
+        temp1->next=NULL;
+        delete temp1;
+        if(temp2==NULL)
+            tail=temp;
+    }
+}
 int main()
 {
-    node *node1=new node;
+    node *node1=new node(5);
+    //node1->data=5;
+    //node1->next=NULL;
     node *head,*tail;
     head=tail=node1;
-    node1->data=5;
-    node1->next=NULL;
     insertAtPos(2,10,head,tail);
     print(head);
     insertAtPos(3,15,head,tail);
@@ -82,6 +131,15 @@ int main()
     print(head);
     insertAtPos(6,20,head,tail);
     print(head);
-
-
+    deleteAt(3,head,tail);
+    print(head);
+    deleteAt(1,head,tail);
+    print(head);cout<<head->data<<" "<<tail->data<<endl;
+    deleteAt(4,head,tail);
+    print(head);cout<<head->data<<" "<<tail->data<<endl;
+    cout<<"adress before delete:"<<head;
+    delete head;
+    cout<<"\t adress after delete:"<<head<<endl;
+    cout<<head->data<<endl;
+    cout<<head->next;
 }
